4d226b7bec49d59c28c1e65dbaefd2e3b138ea84
shazaib491 <shazaibrehman127@gmail.com>
2020-11-02 21:07:51 +0530

    ngForm

--- src/app/osaf/osaf.component.ts
+++ src/app/osaf/osaf.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Student} from '../student'
 @Component({
   selector: 'app-osaf',
   templateUrl: './osaf.component.html',
@@ -12,103 +12,12 @@ export class OsafComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  // setValue(event) {
-  //   const val = event.target.value;
-  //   this.store = val;
-  // }
-  // setFont(operation) {
-  //   if (operation == 'inc') {
-  //     this.i = this.i + 1;
-  //   }
-  //   else if (operation == 'dec') {
-  //     this.i = this.i - 1;
-  //   }
-  // }
-//   color: any;
-//   setColor(param) {
-//     console.log(param.target.value)
-//     this.color = param.target.value;
-//   }
-//   record = {
-//     name:"",
-//     stno:"",
-//     company:"",
-//     salary:""
-//   }
-// val:any;
-//   sendData(event){
-//     const keyname=event.target.name;
-//      this.val=event.target.value;
-//      this.record[keyname]=this.val;
-//
-//     }
 
-
-
-// isSubmitted=false;
-//   submit(){
-//     this.isSubmitted=true
-//
-//     console.log(this.record)
-//
-// }
-//
-//   submits(){
-//     this.isSubmitted=!this.isSubmitted;
-//
-//     this.record = {
-//       name:"",
-//       stno:"",
-//       company:"",
-//       salary:""
-//     }
-//   }
-// isDone:boolean=false
-//   submitss(){
-//     this.isDone=!this.isDone;
-//
-  // }
-  mystyles={
-    width:"100px",
-    height:"100px",
-    color:"white",
-    background:"red",
-    border:"1px solid red"
-  }
-
-  setColor(event){
-    const color=event.target.value
-    this.mystyles['background']=color
-  }
-
-  myClass={
-    border:true,
-    "text-primary":true,
-    "bg-danger":true
-  }
-// setClass(){
-//   this.myClass['text-primary']=!this.myClass['text-primary']
-// }
-
-myclass={
-  "bg-danger":true
+  // ngModel  instance formControl
+   // ngForm track object
+   // (click) (onSubmit)
+setData(value){
+  console.log(value);
 }
-isChecked=true
-SetLike(){
-
-  this.isChecked=!this.isChecked
-  // oppsite true
-  this.myclass['bg-danger']=!this.myclass['bg-danger']
-  // oppsite true
-  this.myclass['text-dark']=!this.myclass['text-dark']
-}
-myobj={
-  "fa fa-thumbs-o-up":true
-}
-setClss(){
-  this.myobj['fa fa-thumbs-o-up']=!this.myobj['fa fa-thumbs-o-up']
-
-}
-
-
+data="data submitted";
 }
---
+++ src/app/student.ts
@@ -0,0 +1,5 @@
+export interface Student {
+  name:String;
+  class:number;
+  rollno:number;
+}
